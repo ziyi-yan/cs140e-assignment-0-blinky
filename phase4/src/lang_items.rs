dc399ff0a4e52f4f070440a077bfaeac8d7a215d
Ziyi Yan <ziyi.yan@foxmail.com>
2020-03-07 18:56:47 +0800

    finish blinky rust
    Signed-off-by: Ziyi Yan <ziyi.yan@foxmail.com>

--- phase4/src/lang_items.rs
+++ phase4/src/lang_items.rs
@@ -1,6 +1,7 @@
 #[lang = "eh_personality"] pub extern fn eh_personality() {}
 
-#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! { loop{} }
+use core::panic::PanicInfo;
+#[panic_handler] #[no_mangle] pub extern fn panic_fmt(_info: &PanicInfo) -> ! { loop{} }
 
 #[no_mangle]
 pub unsafe extern fn memcpy(dest: *mut u8, src: *const u8,
--- phase4/src/lib.rs
+++ phase4/src/lib.rs
@@ -1,8 +1,6 @@
-#![feature(compiler_builtins_lib, lang_items, asm, pointer_methods)]
+#![feature(lang_items, asm)]
 #![no_builtins]
 #![no_std]
-
-extern crate compiler_builtins;
 
 pub mod lang_items;
 
@@ -15,12 +13,21 @@ const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
 #[inline(never)]
 fn spin_sleep_ms(ms: usize) {
     for _ in 0..(ms * 6000) {
-        unsafe { asm!("nop" :::: "volatile"); }
+        unsafe {
+            asm!("nop" :::: "volatile");
+        }
     }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn kmain() {
     // FIXME: STEP 1: Set GPIO Pin 16 as output.
+    *GPIO_FSEL1 = 1 << (3 * 6);
     // FIXME: STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        *GPIO_SET0 = 1 << 16;
+        spin_sleep_ms(100);
+        *GPIO_CLR0 = 1 << 16;
+        spin_sleep_ms(100);
+    }
 }
